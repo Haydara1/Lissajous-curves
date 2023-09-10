b97b95156f97baa9e1a142aa4c7bc552ac056b67
Haydara <haydaraal2005@gmail.com>
2023-09-10 10:00:22 +0200

    Added properties for the equatoin

--- Program.cs
+++ Program.cs
@@ -2,10 +2,43 @@
 
 using Raylib_cs;
 
+
 internal class Program
 {
+    static private int a;
+
+    static public int A
+    {
+        get { return a; }
+        set { a = value; }
+    }
+
+    static private int b;
+
+    static public int B
+    {
+        get { return b; }
+        set { b = value; }
+    }
+
+
     static void Main(string[] args)
     {
+        A = 1;
+        B = 1;
+
         Raylib.InitWindow(1080, 720, "Lissajous Curves");
+
+        while (!Raylib.WindowShouldClose())
+        {
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.BLACK);
+
+            
+
+            Raylib.EndDrawing();
+        }
+
+        Raylib.CloseWindow();
     }
 }
