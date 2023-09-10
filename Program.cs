3adb9e6a27618220b92b0562310bd9a064717081
Haydara <haydaraal2005@gmail.com>
2023-09-10 09:50:23 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Lissajous_curves;
+
+using Raylib_cs;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Raylib.InitWindow(1080, 720, "Lissajous Curves");
+    }
+}
